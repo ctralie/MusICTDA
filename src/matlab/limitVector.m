00544f4f5216187dc4f2229283c16de44368cbf7
julesknee <julia.ni.2010@gmail.com>
2014-07-16 23:56:03 -0400

    Delete limitVector.m

--- src/matlab/limitVector.m
+++
@@ -1,7 +0,0 @@
-function [ limVect ] = limitVector( Trans, n ) 
-%[limVect] = limitVector(Trans) computes the limiting vector from Trans.
-
-limTrans = limitMatrix(Trans,n); 
-limVect = limTrans(1,:); 
-
-end
