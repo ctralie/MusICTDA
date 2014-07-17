08c41aa9dddb120e3ad19293bae51fc09b3ed7eb
julesknee <julia.ni.2010@gmail.com>
2014-07-16 23:54:25 -0400

    updated description

--- src/matlab/absorbProb.m
+++ src/matlab/absorbProb.m
@@ -1,6 +1,8 @@
 function [ absorbMatrix ] = absorbProb( Trans )
-%[absorbMatrix] = absorbProb(Trans) calculates the matrix of absorption 
-%   probabilities absorbMatrix going from row to column state. 
+%ABSORBPROB computes matrix of absorption proabilities for absorbing chain
+%   [absorbMatrix] = absorbProb(Trans) calculates the matrix of absorption 
+%   probabilities, absorbMatrix, of going from each transient state to each
+%   absorbing state using transition matrix Trans. 
 
 [~,~,transAbsorb] = canonicalForm(Trans);
 fundTrans = fundMatrix(Trans);
