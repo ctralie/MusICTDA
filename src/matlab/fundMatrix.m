67de1e9f8a8c55b3a7930cc74a9581899031b01b
julesknee <julia.ni.2010@gmail.com>
2014-07-16 23:55:28 -0400

    updated description

--- src/matlab/fundMatrix.m
+++ src/matlab/fundMatrix.m
@@ -1,8 +1,11 @@
 function [ fundTrans ] = fundMatrix( Trans ) 
-%[fundTrans] = fundMatrix(Trans) computes the fundamental matrix fundTrans
-%   for absorbing chains from the canonical form of a transition matrix Trans. 
-%   Entries in fundTrans give expected number of times from row state to 
-%   column state until absorption. 
+%FUNDMATRIX converts transition matrix for absorbing Markov chains to 
+%fundamental matrix 
+%   [fundTrans] = fundMatrix(Trans) computes fundamental matrix fundTrans
+%   for absorbing chains using canonical form of transition matrix Trans. 
+%   Entries in fundTrans give expected number of times process is in
+%   absorbing state if started in transient state, or the expected time 
+%   required before absorption. 
  
 [~,tranStates,~] = canonicalForm(Trans);
 length = size(tranStates,1);
