9b63f3a2b5b1189e292975e65ac13cc06e3b3bbf
julesknee <julia.ni.2010@gmail.com>
2014-07-17 15:39:30 -0400

    updated code

--- src/matlab/meanRecurrenceTime.m
+++ src/matlab/meanRecurrenceTime.m
@@ -1,8 +1,9 @@
 function [ meanRecVector, meanRecMatrix ] = meanRecurrenceTime( Trans, n ) 
 %MEANRECURRENCETIME computes the mean time to return to a state for the
-%first time for an ergodic chain 
+%first time for a regular, ergodic chain 
 %   [meanRecVector] = meanRecurrenceTime(Trans,n) gives mean recurrence times
-%   in vector meanRecVector and diagonalizes those entries in meanRecMatrix.
+%   in vector meanRecVector and diagonalizes those entries in meanRecMatrix
+%   for n large steps. 
 
 [limVec,~] = limiting(Trans,n);
 meanRecVector = 1./limVec; 
