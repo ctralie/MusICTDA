40a10fe543372abf7aacd4b383720b3907191ea2
julesknee <julia.ni.2010@gmail.com>
2014-07-17 15:39:53 -0400

    updated code

--- src/matlab/limiting.m
+++ src/matlab/limiting.m
@@ -1,11 +1,11 @@
 function [ limVec, limMatrix ] = limiting( Trans, n )
-%LIMITING computes limiting matrix the transition matrix approaches
+%LIMITING computes limiting matrix the transition matrix approaches for
+%regular, ergodic Markov chain
 %   [limVect,limMatrix] = limiting(Trans,n) returns limiting matrix limTrans
-%   and a row limVec that transition matrix Trans tends to after n large 
+%   and a row limVec that regular transition matrix Trans tends to after n large 
 %   steps. 
 
 limMatrix = sparse(Trans^n);
 limVec = limMatrix(1,:); 
 
 end
-
