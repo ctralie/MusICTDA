8cc550096c64787f79b81b4582cc15e148f09607
Roger <rogerzou94@gmail.com>
2014-07-24 12:55:26 -0400

    generate short-time MC feature matrix

---
+++ src/matlab/generateSTFeatureMatrix.m
@@ -0,0 +1,14 @@
+function [ featureMatrix ] = generateSTFeatureMatrix( transitionMatrix )
+%GENERATESTFEATUREMATRIX generate short-time feature vectors for one song.
+
+ysize = size(transitionMatrix, 1);
+xsize = size(transitionMatrix, 2);
+zsize = size(transitionMatrix, 3);
+featureMatrix = NaN(zsize, xsize*(ysize-1));
+
+for i=1:zsize
+    mc = MarkovChain(transitionMatrix(:,:,i), 0:11);
+    featureMatrix(i,:) = mc.getMarkovFeatures();
+end
+
+end
