9a57a48f5a95086e8678ddac60ae343e5ee2ba1d
julesknee <julia.ni.2010@gmail.com>
2014-07-26 14:16:56 -0400

    renamed

--- src/matlab/MarkovChainAll.m
+++ src/matlab/MarkovChainAll.m
@@ -1,37 +1,39 @@
-classdef MarkovChainAll < handle
+classdef MarkovChainAll < handle   %for entire range of notes 
     properties(SetAccess = 'public', GetAccess = 'public')
         transitionMatrix;
+        noteIndex;
     end
 
     methods(Access = 'public')
 
         function this = MarkovChain(varargin)
-            narginchk(1,4);
+            narginchk(2,4);
             tmatrix = varargin{1};
-            if nargin == 1
+            nindex = varargin{2};
+            if nargin == 2
                 this.transitionMatrix = tmatrix;
+                this.noteIndex = nindex;
             elseif nargin == 4
-                nindex = varargin{2};
                 min = varargin{3};
                 max = varargin{4};
-                [ this.transitionMatrix, ~ ] = setTransitionMatrixRange(tmatrix, nindex, min, max);
+                [ this.transitionMatrix, this.noteIndex ] = setTransitionMatrixRange(tmatrix, nindex, min, max);
             end
         end
 
         function [ transMatrix, prob ] = matrixPowers(this, numSteps, initState, finState)
-            [ transMatrix, prob ] = matrixPowers(this.transitionMatrix, numSteps, initState, finState);            
+            [transMatrix, prob] = matrixPowers(this.transitionMatrix, numSteps, initState, finState);            
         end
 
-        function [ finalDistrib ] = probDistribution(this, numSteps, initVec)
+        function [ finalDistrib ] = finalProbDistrib(this, numSteps, initVec)
             finalDistrib = probDistribution(this.transitionMatrix, numSteps, initVec);
         end
 
         function [ limVec, limMatrix ] = limiting(this, numSteps)
-            [ limVec, limMatrix ] = limiting(this.transitionMatrix, numSteps);
+            [limVec, limMatrix] = limiting(this.transitionMatrix, numSteps);
         end
 
         function [ cTrans, tranStates, transAbsorb ] = canonicalForm(this)
-            [ cTrans, tranStates, transAbsorb ] = canonicalForm(this.transitionMatrix);
+            [cTrans, tranStates, transAbsorb] = canonicalForm(this.transitionMatrix);
         end
 
         function [ fundTrans ] = fundMatrix(this)
@@ -55,20 +57,14 @@ classdef MarkovChainAll < handle
         end
 
         function [ meanRecVector, meanRecMatrix ] = meanRecurrenceTime(this, numSteps)
-            [ meanRecVector, meanRecMatrix ] = meanRecurrenceTime(this.transitionMatrix, numSteps);
+            [meanRecVector, meanRecMatrix] = meanRecurrenceTime(this.transitionMatrix, numSteps);
         end
 
         function [ featureVector ] = getMarkovFeatures(this)
             featureVector = getMarkovFeatures(this.transitionMatrix);
         end
 
-        function nextNote = getNext(this, start)
-            nextNote = getNext(this.transitionMatrix, start); 
-        end
-        
-        function noteSequence = getSequence(this, start, count)
-            noteSequence = getSequence(this.transitionMatrix, start, count)
-        end
-        
     end
 end
+
+
