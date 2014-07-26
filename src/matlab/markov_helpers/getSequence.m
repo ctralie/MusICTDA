3c775107e29b4c08d5220f4eec34be21427c7ab2
julesknee <julia.ni.2010@gmail.com>
2014-07-26 02:18:16 -0400

    created helper func

---
+++ src/matlab/markov_helpers/getSequence.m
@@ -0,0 +1,17 @@
+function noteSequence = getSequence( Trans, start, count )
+%GETSEQUENCE returns array of notes in sequence
+%   noteSequence = getSequence(Trans,start,count) takes transition matrix
+%   Trans, starting note start, and entire sequence's note duration count 
+%   to determine sequence of notes noteSequence. start ranges from 0 to 11, 
+%   with 0 being C.
+
+noteSequence = NaN(1,count); 
+noteSequence(1,1) = start; 
+
+for i = 2:count
+    nextNote = getNext(Trans, start);
+    noteSequence(1,i) = nextNote; 
+    start = nextNote; 
+end
+
+end
