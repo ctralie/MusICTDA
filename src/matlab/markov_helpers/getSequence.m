9f9c431f7dc389bd8f70cfa8fc72b254836d8413
Roger <rogerzou94@gmail.com>
2014-07-31 13:57:04 -0400

    edit getsequence comments

--- src/matlab/markov_helpers/getSequence.m
+++ src/matlab/markov_helpers/getSequence.m
@@ -2,16 +2,15 @@ function [ noteSequence ] = getSequence( Trans, start, count )
 %GETSEQUENCE returns array of notes in sequence
 %   noteSequence = getSequence(Trans,start,count) takes transition matrix
 %   Trans, starting note start, and entire sequence's note duration count 
-%   to determine sequence of notes noteSequence. start ranges from 0 to 11, 
-%   with 0 being C.
+%   to determine sequence of notes noteSequence.
 
-noteSequence = NaN(1,count); 
-noteSequence(1,1) = start; 
+    noteSequence = NaN(1,count);
+    noteSequence(1,1) = start;
 
-for i = 2:count
-    nextNote = getNext(Trans, start);
-    noteSequence(1,i) = nextNote; 
-    start = nextNote;
-end
+    for i = 2:count
+        nextNote = getNext(Trans, start);
+        noteSequence(1,i) = nextNote;
+        start = nextNote;
+    end
 
 end
